81214c414bb13245fd8922a2cc41d362c1b253d9
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2017-11-27 15:19:44 -1000

    prob1

--- js/app.js
+++ js/app.js
@@ -4,8 +4,23 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var naV = document.getElementsByClassName("navi");
 
+function showElem(){
+	var see = this.querySelectorAll(".inner")[0];
+	if (see.style.display === "none"){
+		see.style.display = "block";
+	}else{
+		see.style.display = "none";
+	}
 
+}
 
+for(var i =0;i<naV.length;i++){
+	naV[i].addEventListener("click", showElem);
+	console.log(naV[i]);
+}
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+
+var cLasses = document.getElementsByClassName("name");
